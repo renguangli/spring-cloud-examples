a8a10bb47db93820f1812353c84169b287cfd472
renguangli <renguangli@bonc.com.cn>
2018-08-27 18:31:46 +0800

    服务提供者

---
+++ spring-cloud-provider/src/main/java/com/renguangli/provider/CalController.java
@@ -0,0 +1,14 @@
+package com.renguangli.provider;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalController {
+
+    @GetMapping("/add")
+    public String add(@Value("${server.port}") String port, String x, String y) {
+        return "x+y=" + x + y + "," + "the port is " + port;
+    }
+}
