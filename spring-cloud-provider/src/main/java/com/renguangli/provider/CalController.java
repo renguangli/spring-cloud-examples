7d5afe18239a81ae3c89c03cd0ca472da1c8b82f
renguangli <renguangli@bonc.com.cn>
2018-08-29 11:50:54 +0800

    配置中心server/client

--- spring-cloud-provider/src/main/java/com/renguangli/provider/CalController.java
+++ spring-cloud-provider/src/main/java/com/renguangli/provider/CalController.java
@@ -11,4 +11,9 @@ public class CalController {
     public String add(@Value("${server.port}") String port, String x, String y) {
         return "x+y=" + x + y + "," + "the port is " + port;
     }
+
+    @GetMapping("/hello")
+    public String hello(@Value("${hello}") String hello) {
+        return hello;
+    }
 }
